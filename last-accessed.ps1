f845eb81df2c06b6e9c6a80883d093a3ea036f4a
auraria <53339284+auraria@users.noreply.github.com>
2019-07-27 08:00:37 -0400

    V0.1

---
+++ last-accessed.ps1
@@ -0,0 +1,7 @@
+$dirs = "C:\users\*", "C:\temp\"  #Add multiple comma delimited dirs if wanted
+#can also run this against a remote host by placing \\HOSTNAME or IP\C$\
+
+foreach ($d in $dirs){
+    dir $d -r | %{Get-ItemProperty $_ lastaccesstime,creationtime} | Format-Table | out-file last-access.csv
+}
+#outputs to local directory
