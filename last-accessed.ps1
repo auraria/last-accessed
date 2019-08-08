e0228ec278f526551d3d4bcd7aa4d4f872609e90
Dave Williams <53339284+auraria@users.noreply.github.com>
2019-08-08 14:47:56 -0400

    v0.3
    Changed out-file to export csv

--- last-accessed.ps1
+++ last-accessed.ps1
@@ -2,6 +2,6 @@ $dirs = "C:\users\*"  #Add multiple comma delimited dirs if wanted
 #can also run this against a remote host by placing \\HOSTNAME or IP\C$\
 
 foreach ($d in $dirs){
-    dir $d -r | %{Get-ItemProperty $_ lastaccesstime,creationtime} | Format-Table | out-file last-access.csv
+    dir $d -r | %{Get-ItemProperty $_ lastaccesstime,creationtime} | export-csv last-access.csv
 }
 #outputs to local directory
